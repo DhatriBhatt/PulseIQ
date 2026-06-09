4a1020967ccb2f37fbb7ffe1926e4b0f5201584d
DhatriBhatt <dhatribhatt0510@gmail.com>
2026-06-09 17:56:53 -0400

    Format animated KPI counters

--- web/script.js
+++ web/script.js
@@ -6,3 +6,38 @@ navLinks.forEach(link => {
         link.classList.add("active");
     }
 });
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const current = +counter.innerText.replace(/[^0-9]/g, "");
+        const increment = target / 100;
+
+        if(current < target){
+            let value = Math.ceil(current + increment);
+
+            if(counter.dataset.type === "percent"){
+                counter.innerText = value + "%";
+            }else if(counter.dataset.type === "money"){
+                counter.innerText = "$" + value + "K";
+            }else if(counter.dataset.type === "number"){
+                counter.innerText = value.toLocaleString();
+            }
+
+            setTimeout(updateCounter, 20);
+        }else{
+            if(counter.dataset.type === "percent"){
+                counter.innerText = target + "%";
+            }else if(counter.dataset.type === "money"){
+                counter.innerText = "$" + target + "K";
+            }else if(counter.dataset.type === "number"){
+                counter.innerText = target.toLocaleString();
+            }
+        }
+    };
+
+    updateCounter();
+});
